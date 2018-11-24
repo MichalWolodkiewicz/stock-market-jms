f27e6ff3cd0a413c317934bdd8d18618aab80455
MichalWolodkiewicz <michal@quipu.software>
2018-11-24 21:18:15 +0100

    consumer connection has clientId.

--- stock-consumer/src/main/java/com/quipu/stock/consumer/AppConfig.java
+++ stock-consumer/src/main/java/com/quipu/stock/consumer/AppConfig.java
@@ -1,5 +1,6 @@
 package com.quipu.stock.consumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +8,21 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.jms.ConnectionFactory;
+import java.util.UUID;
 
 @Configuration
 @EnableScheduling
 @EnableJms
 public class AppConfig {
+
+    @Value("${jms.clientIdPrefix}")
+    private String clientIdPrefix;
 
     @Bean
     @Profile("directDestinationEnabled")
@@ -29,12 +35,17 @@ public class AppConfig {
 
     @Bean
     @Profile("topicDestinationEnabled")
-    JmsListenerContainerFactory topicJmsConnectionFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+    JmsListenerContainerFactory topicJmsConnectionFactory(SingleConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        connectionFactory.setClientId(generateClientPrefix());
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setMessageConverter(jacksonJmsMessageConverter());
         configurer.configure(factory, connectionFactory);
         factory.setPubSubDomain(true);
         return factory;
+    }
+
+    private String generateClientPrefix() {
+        return clientIdPrefix + "-" + UUID.randomUUID().toString().substring(0, 4);
     }
 
     @Bean
